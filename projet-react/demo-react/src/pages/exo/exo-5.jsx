04bcbb5ba1f86336bae5a1399c2c52f082b60d15
zacharyb12 <boualemzachary89@yahoo.fr>
2025-10-30 11:14:20 +0100

    exo-communication

---
+++ projet-react/demo-react/src/pages/exo/exo-5.jsx
@@ -0,0 +1,31 @@
+// Todo List
+
+// Tache :
+// - id (number), 
+// - Nom ( string ) , 
+// - priorité (basse , normal , urgent) , 
+// - un status (booleen)
+
+// 1. Le composant parent implemente les actions et la liste
+//-  Ajouter
+//- Modifier
+//- Supprimer 
+
+
+// 2. Un composant enfant pour ajouter une tache : 
+// -formulaire avec soumission
+// - Appel de la fonction du parent : ajouter
+
+// 3. Un composant enfant pour afficher la liste des taches : 
+// - Appel des fonctions du parent :Modifier le status,supprimer une tache
+// - Si la tache est urgente , l'afficher en rouge
+// - Si la tache est normale , l'afficher en vert
+// - Si la tache est basse , l'afficher en bleu
+// Si la tache est terminée , l'afficher en gris
+// Modifier 
+// supprimer
+
+// 4. Bonus :
+// Composant Pour afficher un element de la liste
+// Composant pour afficher la liste
+// Affichage avec style
