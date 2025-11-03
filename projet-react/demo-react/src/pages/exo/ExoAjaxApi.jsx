975bafefccff8c7b21f2fbde1d8c3a4ee6421e58
zacharyb12 <boualemzachary89@yahoo.fr>
2025-11-03 11:56:40 +0100

    exo api

---
+++ projet-react/demo-react/src/pages/exo/ExoAjaxApi.jsx
@@ -0,0 +1,11 @@
+// Adresse API:
+
+// https://sampleapis.com/api-list
+
+// https://github.com/public-apis/public-apis
+
+// Creer un nouveau projet 
+// implementer la navigation 
+// ajouter une page d'accueil
+
+// ajouter une page par requette 
